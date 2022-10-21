c62af191b39dd2252a363afb5cd244adb29fa439
Abhishek Singh <abhishek805483as@gmail.com>
2022-10-21 20:02:49 +0530

    added more conditions

--- app.js
+++ app.js
@@ -15,18 +15,31 @@ calculateProfitAndLoss(ip , qty, crr)
 
 
 function calculateProfitAndLoss(initial,quantity,current) {
-    if(initial>current) {
+    if(initial < 0 || current < 0 || quantity < 0 ) {
+        showOutput("Please enter positive numerical values only");
+           outputEl.style.display="block";
+           outputEl.style.color="red"
+
+    } else if (initial === 0 || current === 0 || quantity === 0) {
+            showOutput("Please enter non-zero numerical values only");
+            outputEl.style.display="block";
+            outputEl.style.color="red"
+
+    } else if (current>initial) {
+       var profit = (current-initial) * quantity
+       var profitPercentage = (profit/(initial * quantity)) * 100 
+    showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage.toFixed(2)}%`)
+       outputEl.style.display="block";
+       outputEl.style.color="green"
+
+    } else if (initial>current) {
         var loss = (initial-current) * quantity
-        var lossPercentage = (loss/initial) * 100
+        var lossPercentage = (loss/(initial * quantity)) * 100
         showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage.toFixed(2)}%`)
         outputEl.style.display="block";
         outputEl.style.color="red"
-    } else if (current>initial) {
-        var profit = (current-initial) * quantity
-    var profitPercentage = (profit/initial) * 100 
-    showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage.toFixed(2)}%`)
-    outputEl.style.display="block";
-        outputEl.style.color="green"
+    
+    
     } else {
         showOutput(`No pain No gain`)
     }
@@ -39,7 +52,7 @@ function calculateProfitAndLoss(initial,quantity,current) {
 
 
 
-
+ 
 
 
 
